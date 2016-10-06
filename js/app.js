b730c6e0002f9f8321ad00bb3f1a52a011a74f48
jayedward21 <jay.moreno21@gmail.com>
2016-10-06 15:56:39 -0600

    Project Completed

--- js/app.js
+++ js/app.js
@@ -1,29 +1,50 @@
-$(document).ready(function() {
-  $('.ryu').mouseenter(function() {
-    $('.ryu-still').hide();
-    $('.ryu-ready').show();
-  })
-  .mouseleave(function() {
-    $('.ryu-ready').hide();
-    $('.ryu-still').show();
-  })
-  .mousedown(function() {
-    // play hadouken sound
-    $('.ryu-ready').hide();
-    $('.ryu-throwing').show();
-    $('.hadouken').finish().show()
-  .animate(
-    {'left': '1020px'},
-    500,
-    function() {
-      $('.hadouken').hide();
-      $('.hadouken').css('left', '520px');
-    }
-  );
-    // animate hadouken to the right of the screen
-  })
-  .mouseup(function() {
-    $('.ryu-throwing').hide();
-    $('.ryu-ready').show();
-  });
+$(document).ready(function(){
+    $('.ryu').mouseenter(function(){
+        $('.ryu-still').hide();
+        $('.ryu-ready').show();
+    })
+    .mouseleave(function() {
+        $('.ryu-still').show();
+        $('.ryu-ready').hide();
+    })
+    .keydown(function (e) {
+        if (e.keyCode == 88) {
+            $('#Energy-sound')[0].play(30);
+            $('.ryu-ready').hide();
+            $('.ryu-still').hide();
+            $('.ryu-throwing').hide();
+            $('.ryu-cool').show();
+        }
+    })
+    .keyup(function (e) {
+        if (e.keyCode == 88) {
+            $('#Energy-sound')[0].pause();
+            $('.ryu-cool').hide();
+            $('.ryu-ready').show();
+        };
+    })
+    .mousedown(function(){
+        playHadouken();
+        $('.ryu-ready').hide();
+        $('.ryu-cool').hide();
+        $('.ryu-still').hide();
+        $('.ryu-throwing').show();
+        $('.hadouken').finish().show().animate(
+            {'left': '1020px'},
+            500,
+            function(){
+                $('.hadouken').hide();
+                $('.hadouken').css('left', '520px');
+            });
+    })
+    .mouseup(function(){
+        $('.ryu-throwing').hide();
+        $('.ryu-ready').show();
+    });
 });
+
+function playHadouken() {
+    $('#hadouken-sound')[0].volume = 0.5;
+    $('#hadouken-sound')[0].load();
+    $('#hadouken-sound')[0].play();
+}
