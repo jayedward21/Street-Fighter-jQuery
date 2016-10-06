a72820d7d5d6b90f2b7491a34fe954e7c69319ec
jayedward21 <jay.moreno21@gmail.com>
2016-10-06 10:58:06 -0600

    Project Update

---
+++ js/app.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+  $('.ryu').mouseenter(function() {
+    $('.ryu-still').hide();
+    $('.ryu-ready').show();
+  })
+  .mouseleave(function() {
+    $('.ryu-ready').hide();
+    $('.ryu-still').show();
+  })
+  .mousedown(function() {
+    // play hadouken sound
+    $('.ryu-ready').hide();
+    $('.ryu-throwing').show();
+    $('.hadouken').finish().show()
+  .animate(
+    {'left': '1020px'},
+    500,
+    function() {
+      $('.hadouken').hide();
+      $('.hadouken').css('left', '520px');
+    }
+  );
+    // animate hadouken to the right of the screen
+  })
+  .mouseup(function() {
+    $('.ryu-throwing').hide();
+    $('.ryu-ready').show();
+  });
+});
